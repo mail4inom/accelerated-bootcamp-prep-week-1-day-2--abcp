515a73e22f647c3a231d3770a98b6715f5bbcca7
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-23 23:13:45 +0000

    Done.

--- array.js
+++ array.js
@@ -1,34 +1,43 @@
 function addElementToBeginningOfArray(array, element) {
- // add the element to the beginning of the array without mutating the array
+  return [element, ...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
-  // add element to the beginning of the array by mutating the array
+function destructivelyAddElementToBeginningOfArray(array, element) {
+array.unshift(element);
+return array;
 }
 
 function addElementToEndOfArray(array, element) {
-
+return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element);
+  return array;
 }
-
 function accessElementInArray(array, index) {
+   return array[index];
 
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
 
 }
 
-function removeElementFromBeginningOfArray(array) {
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
 }
 
-function destructivelyRemoveElementFromEndOfArray(array) {
-
-}
-
-function removeElementFromEndOfArray(array) {
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length -1);
 
 }
